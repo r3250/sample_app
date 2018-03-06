653b9030df94cfd0b0d071d5aabbf612d4a109a1
r3250 <n0325.0329@gmail.com>
2018-03-06 12:39:22 +0000

    Start3-1~8>

---
+++ config/routes.rb
@@ -0,0 +1,17 @@
+Rails.application.routes.draw do
+
+    get 'posts/new'
+
+	get '/top' => 'root#top'
+
+	post '/posts' => 'posts#create'
+
+	get '/posts/:id' => 'posts#show', as: 'post'
+
+	get '/posts' => 'posts#index'
+
+	get '/posts/:id/edit' => 'posts#edit', as: 'edit_post'
+
+	patch '/posts/:id' => 'posts#update', as: 'update_post'
+
+end
