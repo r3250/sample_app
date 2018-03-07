2d4d65c81b144c199196199e8ef2ea5a39a6490b
r3250 <n0325.0329@gmail.com>
2018-03-07 04:27:29 +0000

    Finish3-9

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -25,6 +25,11 @@ class PostsController < ApplicationController
 	  	     post.update(post_params)
 	  	     redirect_to post_path(post.id)
 	  end
+	  def    destroy
+	  		 post = Post.find(params[:id])
+	  		 post.destroy
+	  		 redirect_to posts_path
+	  end
 
 	  private
 
